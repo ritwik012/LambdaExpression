28bbbb4dcfb319a6e8081de1fd39fcd483f5caaa
“ritwik012” <rutwikgulhaneg@gmail.com>
2021-12-06 23:30:00 +0530

    [Rutwik]: Added Person Data File

---
+++ LambdaExpression/DataManagement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PersonDataManagement
+{
+    public class DataManagement
+    {
+        public void AddRecords(List<Person> listPersonsInCity)
+        {
+            listPersonsInCity.Add(new Person("203456876", "John", "12 Main street", 15));
+            listPersonsInCity.Add(new Person("203456877", "Sam", "12 Main street", 25));
+            listPersonsInCity.Add(new Person("203456878", "Elan", "12 Main street", 35));
+            listPersonsInCity.Add(new Person("203456879", "Smith", "12 Main street", 45));
+            listPersonsInCity.Add(new Person("203456880", "Sam", "12 Main street", 55));
+            listPersonsInCity.Add(new Person("203456881", "Sue", "12 Main street+", 65));
+            listPersonsInCity.Add(new Person("203456882", "Winston", "1208 Alex St, Newyork,NY", 65));
+            listPersonsInCity.Add(new Person("203456883", "Mac", "126 Province Ave,Baltimore,NY", 85));
+            listPersonsInCity.Add(new Person("203456884", "Sam", "126 Province Ave,Baltimore,NY", 95));
+        }
+    }
+}
---
+++ LambdaExpression/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PersonDataManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Person Data Management");
+            DataManagement data = new DataManagement();
+            List<Person> listPersonsInCity = new List<Person>();
+            data.AddRecords(listPersonsInCity);
+        }
+    }
+}
